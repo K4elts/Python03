7cd6f6c948324b5e751025bba90ce57b12f1f04f
K4elts <andreslt310@gmail.com>
2026-09-14 16:48:42 +0200

    finish ex5

--- ex5/ft_data_stream.py
+++ ex5/ft_data_stream.py
@@ -2,7 +2,7 @@ import typing
 import random
 
 
-def gen_event() -> typing.Generator:
+def gen_event() -> typing.Generator[tuple[str, str], None, None]:
     player_list = ["alice",
                    "bob",
                    "charlie",
@@ -24,18 +24,29 @@ def gen_event() -> typing.Generator:
         yield player, action
 
 
-if __name__ == "__main__":
-    print("=== Game Data Stream Processor ===")
-    for i in range(0, 1000 + 1):
+def consume_event(
+        event_list: list[tuple[str, str]]
+        ) -> typing.Generator[tuple[str, str], None, None]:
+    while event_list:
+        event_choice = random.choice(event_list)
+        event_list.remove(event_choice)
+        yield event_choice
+
+
+def main() -> None:
+    for i in range(0, 1000):
         player, action = next(gen_event())
         print(f"Event {i}: Player {player} did action {action}")
     event_list = []
     for i in range(0, 10):
-        player, event = next(gen_event())
-        event_list.append((player, event))
+        event = next(gen_event())
+        event_list.append(event)
     print(f"Built list of 10 events: {event_list}")
-    for i in range(0, 10):
-        event_choice = random.choice(event_list)
+    for event_choice in consume_event(event_list):
         print(f"Got event from list: {event_choice}")
-        event_list.remove(event_choice)
         print(f"Remains in list: {event_list}")
+
+
+if __name__ == "__main__":
+    print("=== Game Data Stream Processor ===")
+    main()
